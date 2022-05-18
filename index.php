a1c3af63dccb0da487cbe3877f37194bf3f60553
OliverHG <79026110+OliverHG@users.noreply.github.com>
2022-05-18 12:54:11 +1200

    Pre-Coding

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="section group">
+  <div class="col span_1_of_3">
+  This is column 1
+  </div>
+  <div class="col span_1_of_3">
+  This is column 2
+  </div>
+  <div class="col span_1_of_3">
+  This is column 3
+  </div>
+</div>
+
+<div class="section group">
+  <div class="col span_1_of_3">
+  This is column 1
+  </div>
+  <div class="col span_1_of_3">
+  This is column 2
+  </div>
+  <div class="col span_1_of_3">
+  This is column 3
+  </div>
+</div>
+
+<div class="section group">
+  <div class="col span_1_of_3">
+  This is column 1
+  </div>
+  <div class="col span_1_of_3">
+  This is column 2
+  </div>
+  <div class="col span_1_of_3">
+  This is column 3
+  </div>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
