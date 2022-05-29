80ba5b029c016109b7af70b73113ada9c5b44055
OliverHG <79026110+OliverHG@users.noreply.github.com>
2022-05-30 09:45:48 +1200

    Version 1.001

--- index.php
+++ index.php
@@ -6,39 +6,79 @@
 <body>
 
 <div class="section group">
-  <div class="col span_1_of_3">
-  Logo
+  <div class="col span_1_of_3 header">
+  <!-- Header Left -->
+
+  <img class="logo" src="Images/logonobg.png" alt="Company Logo" width="25%">
+
   </div>
-  <div class="col span_1_of_3">
-  Header Left
+
+  <div class="col span_2_of_3 header">
+  <!-- Header Left -->
+
+<div class="linksdiv">    <!-- Links in Nav -->
+
+  <p> Home </p>
+
+  <p> Products </p>
+
+  <p> Contact </p>
+
+  <p> Cart </p>
+
+ </div> <!-- End of Links in Nav -->
+
   </div>
-  <div class="col span_1_of_3">
-  Header Right
-  </div>
+
+
+
+
 </div>
 
 <div class="section group">
   <div class="col span_1_of_3">
-  Main Body Left
+  <!--  Main Body Left -->
+
   </div>
+
   <div class="col span_1_of_3">
-  Main Body Centre
+  <!-- Main Body Centre -->
+
   </div>
+
+
+
   <div class="col span_1_of_3">
-  Main Body Right
+  <!-- Main Body Right -->
+
   </div>
+
+
+
 </div>
 
 <div class="section group">
   <div class="col span_1_of_3">
-  Footer
+  <!-- Footer Left -->
+
   </div>
+
+
+
   <div class="col span_1_of_3">
-  Footer
+  <!-- Footer Centre -->
+
   </div>
+
+
+
   <div class="col span_1_of_3">
-  Footer
+  <!-- Footer Right -->
+
   </div>
+
+
+
 </div>
 
 </body>
