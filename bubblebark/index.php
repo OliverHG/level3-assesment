f38dcc3d7c7d0c882fcf2b118794b9e3cd686c39
OliverHG <79026110+OliverHG@users.noreply.github.com>
2022-07-06 12:12:53 +1200

    Turned Footer into a Include Page

--- bubblebark/index.php
+++ bubblebark/index.php
@@ -194,17 +194,7 @@
     </div>
 
     
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <p>Copyright © 2022 Oliver Graham</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+<?php include 'footer.php';?>
 
 
     <!-- Bootstrap core JavaScript -->
